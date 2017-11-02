37573b8cb06992c84cb72df7af6a7af150664f0d
beepscore <support@beepscore.com>
2017-11-02 14:12:41 -0700

    Add functions

--- app.js
+++ app.js
@@ -4,5 +4,19 @@ var person = {
     age: 28
 };
 
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function noExplicitReturnReturnsUndefined(a, b) {
+}
+
 console.log(person);
+// { firstName: 'Bucky', lastName: 'Roberts', age: 28 }
+
+console.log(addNumbers(7, 3));
+// 10
+
+console.log(noExplicitReturnReturnsUndefined());
+// undefined
 
