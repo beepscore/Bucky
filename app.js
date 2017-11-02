27e2a79ae1074f6d8b258c95bb1516b46c705dad
beepscore <support@beepscore.com>
2017-11-02 14:58:22 -0700

    comment out previous code, add placeAnOrder and cookAndDeliverFood(callback)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/**
 var person = {
     firstName: "Bucky",
     lastName: "Roberts",
@@ -12,7 +13,7 @@ function noExplicitReturnReturnsUndefined(a, b) {
 }
 
 // right hand side is anonymous function, assign it to variable printBacon
-var printBacon = function() {
+var printBacon = function () {
     console.log("Bacon is healthy. Don't believe the doctors!");
 };
 
@@ -31,3 +32,28 @@ printBacon();
 // after timeout execute function, timeout in msec
 // here must not append () after printBacon
 setTimeout(printBacon, 2000);
+
+ */
+
+function placeAnOrder(orderNumber) {
+    console.log("Customer order:", orderNumber);
+    // javascript style anonymous function insert space before ()
+    cookAndDeliverFood(function () {
+        // captures orderNumber from outer scope
+        console.log("Delivered food order:", orderNumber);
+    });
+}
+
+// when async operation completes, runs callback supplied by caller
+function cookAndDeliverFood(callback) {
+
+    // simulate a slow operation e.g. read from a database
+    // schedule callback to run after timeout
+    setTimeout(callback, 2000);
+}
+
+// Simulate users web requests
+placeAnOrder(0);
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
