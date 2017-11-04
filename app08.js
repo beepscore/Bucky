7521b3265bcfd1f96a6431aea7a0e8bc01e8c5b2
beepscore <support@beepscore.com>
2017-11-04 14:08:57 -0700

    Rename movies.js to movies08.js

--- app08.js
+++ app08.js
@@ -1,6 +1,6 @@
 // To fix webstorm warning "unresolved function or method require",
 // in WebStorm preferences / languages and frameworks / Node.js and npm select enable
-var movies = require('./movies');
+var movies = require('./movies08');
 
 // use module namespace
 movies.avatar();
