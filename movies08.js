bff49524e01ccc902ba5faabb3bead238fb4e33d
beepscore <support@beepscore.com>
2017-11-04 14:19:15 -0700

    Add comments

--- movies08.js
+++ movies08.js
@@ -1,5 +1,7 @@
 // Module provides a namespace
 // separate files improve separation of concerns
+// every file implicitly creates an empty module.exports
+// module.exports = {};
 
 function printAvatar() {
     console.log("Avatar: PG-13");
@@ -9,4 +11,5 @@ function printChappie() {
     console.log("Chappie: R");
 }
 
+// add to module.exports using assignment = syntax (compare with movies09.js)
 module.exports.avatar = printAvatar;
