25a36c2cc82b18de081dcbdd7211b7bca5034229
beepscore <support@beepscore.com>
2017-12-08 22:48:24 -0800

    Add comments

--- 13_basic_server/server13.js
+++ 13_basic_server/server13.js
@@ -1,13 +1,20 @@
 var http = require('http');
 
+// callback method
+// in terminal start server via node server13.js
+// in browser enter ip address 0.0.0.0:8888 or localhost:8888
+// terminal log shows browser makes 2 requests
+// A user made a request/
+// A user made a request/favicon.ico
 function onRequest(request, response) {
+
     console.log("A user made a request" + request.url);
+
     response.writeHead(200,  {"Context-Type": "text/plain"});
     response.write("Here is some data");
     response.end();
 }
 
 // argument onRequest is the requestListener
-// browser ip address 0.0.0.0:8888
 http.createServer(onRequest).listen(8888);
 console.log("Node server is now running...");
