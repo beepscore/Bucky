83da94cfa3db8f5b965a9e3d5978ff23185cfbe2
beepscore <support@beepscore.com>
2017-12-10 13:03:49 -0800

    Use middleware in a stack

--- basic_server/server15.js
+++ basic_server/server15.js
@@ -6,5 +6,20 @@ var http = require('http');
 
 var app = connect();
 
+function middleware1(request, response, next) {
+    console.log("Bacon");
+    // use next method on the stack
+    next();
+}
+
+function middleware2(request, response, next) {
+    console.log("Tuna");
+    next();
+}
+
+// use middleware in a stack
+app.use(middleware1);
+app.use(middleware2);
+
 http.createServer(app).listen(8888);
 console.log("Server is running...");
